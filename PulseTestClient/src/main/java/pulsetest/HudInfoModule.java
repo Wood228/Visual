2b8f4894045038d394b8e8c835ec17ca7aee8f0e
Wood228 <162610922+Wood228@users.noreply.github.com>
2026-09-16 17:01:56 +0500

    Add HUD info module

---
+++ PulseTestClient/src/main/java/pulsetest/HudInfoModule.java
@@ -0,0 +1,5 @@
+package pulsetest;
+
+public final class HudInfoModule extends Module {
+    public HudInfoModule() { super("HUD Info", "Coordinates, FPS, ping and speed widget.", Category.HUD); }
+}
